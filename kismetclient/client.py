9aed84c351eb3faa8ee673cd795dbdd94e8d7be1
Paul McMillan <paul.mcmillan@nebula.com>
2012-09-16 14:23:26 -0700

    merge protocols into client

--- kismetclient/client.py
+++ kismetclient/client.py
@@ -4,13 +4,52 @@ import subprocess
 import logging
 
 from kismetclient import handlers
-from kismetclient import protocol
 from kismetclient.utils import get_csv_args
 from kismetclient.utils import get_pos_args
 
 log = logging.getLogger(__name__)
 log.addHandler(logging.StreamHandler())
 log.setLevel(logging.DEBUG)
+
+
+class Command(object):
+    # assign at the class level, so these are unique.
+    # FIXME race condition.
+    command_id = 0
+
+    def __init__(self, command, *opts):
+        Command.command_id += 1
+        self.command_id = Command.command_id
+        self.command = command
+
+        def wrap(opt):
+            if ' ' in opt:
+                return '\x01%s\x01'
+            else:
+                return opt
+        self.opts = map(wrap, opts)
+
+    def __str__(self):
+        return '!%d %s %s\n' % (self.command_id,
+                                self.command,
+                                ' '.join(self.opts))
+
+
+class Response(object):
+    protocol = ''
+    fields = []
+
+    def __init__(self, res):
+        if not res.startswith('*'):
+            raise ValueError('Attempted to create a Response object '
+                             'from string which did not start with "*"')
+        self.protocol, _, tail = res[1:].partition(':')
+        fields = re.findall(' \x01(.*?)\x01| ([^ ]+)', tail)
+        # only one of the regex fields will match; the other will be empty
+        self.fields = [''.join(f) for f in fields]
+
+    def __str__(self):
+        return '*%s: %s' % (self.protocol, str(self.fields))
 
 
 class Client(object):
@@ -53,14 +92,14 @@ class Client(object):
             self.cmd('ENABLE', protocol, fields)
 
     def cmd(self, command, *opts):
-        cmd = protocol.Command(command, *opts)
+        cmd = Command(command, *opts)
         log.debug(cmd)
         self.in_progress[str(cmd.command_id)] = cmd
         self.file.write(cmd)
 
     def read(self):
         line = self.file.readline().rstrip('\n')
-        r = protocol.Response(line)
+        r = Response(line)
         handler = self.handlers.get(r.protocol)
         if handler:
             fields = r.fields
